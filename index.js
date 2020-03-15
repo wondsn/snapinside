6794e1044003319f2fc857d3b6b58f091b3a8d5c
gyeongsoo_kim <kimgs0725@gmail.com>
2020-03-15 16:38:15 +0900

    snapinside 기능 추가
      - logger 추가
      - main.js 라우터 호출 방식 변경
      - 코드 수정

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const session = require('express-session');
-const mongoose = require('mongoose');
+const logger = require('morgan');
+const methodOverride = require('method-override');
+
 const account = require('./model/account');
+const app = express();
 const db_url = 'mongodb+srv://test_username:z1x2c3a4s5d6@cluster0-rd3uc.mongodb.net/snapinside_db?retryWrites=true&w=majority';
+
+// DB Setting
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+var db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', () => {
+  console.log("Connected to mongod server");
+});
+mongoose.connect(db_url);
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
-
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -17,15 +31,10 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }));
+app.use(methodOverride('_method'));
+app.use(logger('short'));
 
-var db = mongoose.connection;
-db.on('error', console.error);
-db.once('open', () => {
-  console.log("Connected to mongod server");
-});
-mongoose.connect(db_url);
-
-var router = require('./router/main')(app, account);
+app.use('/', require('./router/main'));
 
 var server = app.listen(3000, () => {
   console.log("Express server has started on port 3000");
--- router/main.js
+++ router/main.js
@@ -1,150 +1,154 @@
-module.exports = (app, Account) => {
-  app.get('/', (req, res) => {
+const express = require('express');
+const router = express.Router();
+const Account = require('../model/account');
+
+router.get('/', (req, res) => {
+  res.redirect('login');
+});
+
+router.get('/signup/edit', (req, res) => {
+  res.render('signup');
+});
+
+router.post('/signup', async (req, res) => {
+  const usernameRegex = /^[a-z0-9]+$/;
+
+  if (!usernameRegex.test(req.body.userid)) {
+    return res.status(400).json({
+      error: "BAD USERNAME",
+      code: 1
+    });
+  }
+
+  if (req.body.password.length < 4 || typeof req.body.password !== "string") {
+    return res.status(400).json({
+      error: "BAD PASSWORD",
+      code: 2
+    });
+  }
+
+  try {
+    const exists = await Account.findOne({userid: req.body.userid});
+    if (exists) {
+      return res.status(409).json({
+        error: "USERNAME EXISTS",
+        code: 3
+      });
+    }
+    const account = new Account({
+      userid: req.body.userid,
+      password: req.body.password,
+      nickname: req.body.nickname
+    });
+    account.password = account.generateHash(account.password);
+    await account.save();
     res.redirect('login');
-  });
+  } catch (error) {
+    throw error;
+  }
+});
 
-  app.get('/signup/edit', (req, res) => {
-    res.render('signup');
-  });
+router.get('/welcome', (req, res) => {
+  if (!req.session.name) {
+    return res.redirect('/login');
+  } else {
+    res.render('welcome', {name: req.session.name});
+  }
+});
 
-  app.post('/signup', async (req, res) => {
-    const usernameRegex = /^[a-z0-9]+$/;
+router.get('/login', (req, res) => {
+  if (!req.session.name) {
+    res.render('login', {message: 'input your id and password'});
+  } else {
+    res.redirect('/welcome');
+  }
+});
 
-    if (!usernameRegex.test(req.body.userid)) {
-      return res.status(400).json({
-        error: "BAD USERNAME",
-        code: 1
-      });
-    }
+router.post('/login', async (req, res) => {
+  if (typeof req.body.password !== "string") {
+    return res.status(401).json({
+      error: "LOGIN FAILED",
+      code: 1
+    });
+  }
 
-    if (req.body.password.length < 4 || typeof req.body.password !== "string") {
-      return res.status(400).json({
-        error: "BAD PASSWORD",
-        code: 2
-      });
-    }
-
-    try {
-      const exists = await Account.findOne({userid: req.body.userid});
-      if (exists) {
-        return res.status(409).json({
-          error: "USERNAME EXISTS",
-          code: 3
-        });
-      }
-      const account = new Account({
-        userid: req.body.userid,
-        password: req.body.password,
-        nickname: req.body.nickname
-      });
-      account.password = account.generateHash(account.password);
-      await account.save();
-      res.redirect('login');
-    } catch (error) {
-      throw error;
-    }
-  });
-
-  app.get('/welcome', (req, res) => {
-    if (!req.session.name) {
-      return res.redirect('/login');
-    } else {
-      res.render('welcome', {name: req.session.name});
-    }
-  });
-
-  app.get('/login', (req, res) => {
-    if (!req.session.name) {
-      res.render('login', {message: 'input your id and password'});
-    } else {
-      res.redirect('/welcome');
-    }
-  });
-
-  app.post('/login', async (req, res) => {
-    if (typeof req.body.password !== "string") {
+  try {
+    const account = await Account.findOne({userid: req.body.username});
+    if (!account) {
       return res.status(401).json({
         error: "LOGIN FAILED",
         code: 1
       });
     }
-
-    try {
-      const account = await Account.findOne({userid: req.body.username});
-      if (!account) {
-        return res.status(401).json({
-          error: "LOGIN FAILED",
-          code: 1
-        });
-      }
-      if (account.validateHash(req.body.password)) {
-        req.session._id = account._id;
-        req.session.userid = account.userid;
-        req.session.name = account.nickname;
-        await req.session.save();
-        res.redirect('welcome');
-      } else {
-        res.render('login', {message: "please check your password"});
-      }
-    } catch (error) {
-      throw error;
+    if (account.validateHash(req.body.password)) {
+      req.session._id = account._id;
+      req.session.userid = account.userid;
+      req.session.name = account.nickname;
+      await req.session.save();
+      res.redirect('welcome');
+    } else {
+      res.render('login', {message: "please check your password"});
     }
-  });
+  } catch (error) {
+    throw error;
+  }
+});
 
-  app.get('/logout', (req, res) => {
-    if (typeof req.session._id !== "undefined") {
-      req.session.destroy(err => {
-        if (err) throw err;
-      });
-      return res.redirect('/');
-    }
-  });
+router.get('/logout', (req, res) => {
+  if (typeof req.session._id !== "undefined") {
+    req.session.destroy(err => {
+      if (err) throw err;
+    });
+    return res.redirect('/');
+  }
+});
 
-  app.get('/getInfo', (req, res) => {
-    if (typeof req.session._id === "undefined") {
-      return res.status(401).json({ error: 1});
-    }
-    res.json({ userid: req.session.userid });
-  });
+router.get('/getInfo', (req, res) => {
+  if (typeof req.session._id === "undefined") {
+    return res.status(401).json({ error: 1});
+  }
+  res.json({ userid: req.session.userid });
+});
 
-  app.delete('/signout', (req, res) => {
-    if (!req.session._id) {
-      return res.status(402).json({
-        error: "NO SESSION",
+router.delete('/signout', (req, res) => {
+  if (!req.session._id) {
+    return res.status(402).json({
+      error: "NO SESSION",
+      code: 1
+    });
+  }
+
+  Account.remove({ _id: req.session._id }, (err, output) => {
+    if (err) {
+      return res.status(500).json({
+        error: "DATABASE ERROR",
         code: 1
       });
     }
 
-    Book.remove({ _id: req.session._id }, (err, output) => {
-      if (err) {
-        return res.status(500).json({
-          error: "DATABASE ERROR",
-          code: 1
-        });
-      }
+    if (!output.result.n) {
+      return res.status(404).json({
+        error: "USER NOT FOUND",
+        code: 1
+      });
+    }
 
-      if (!output.result.n) {
-        return res.status(404).json({
-          error: "USER NOT FOUND",
-          code: 1
-        });
-      }
-
-      res.json({ message: "USER INFO DELETED"});
-    });
-
-    req.session.destroy(err => {
-      if (err) {
-        throw err;
-      } else {
-        res.redirect('/');
-      }
-    })
+    res.json({ message: "USER INFO DELETED"});
   });
 
-  app.get('/userlist', (req, res) => {
-    Account.find((err, account) => {
-      res.json(account)
-    });
+  req.session.destroy(err => {
+    if (err) {
+      throw err;
+    } else {
+      res.redirect('/');
+    }
   });
-}
+});
+
+router.get('/userlist', (req, res) => {
+  Account.find((err, account) => {
+    res.json(account)
+  });
+});
+
+module.exports = router;
